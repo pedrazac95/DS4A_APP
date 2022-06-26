58c1b823175a99f0e3199f130d68d5003d82337f
Camilo Pedraza <86867284+pedrazac95@users.noreply.github.com>
2022-06-26 12:06:07 -0500

    Delete callbacks.py

--- callbacks.py
+++
@@ -1,9 +0,0 @@
-import dash
-from dash import dcc, html, Input, Output, callback
-
-def register_callbacks(app):
-        
-        #@app.callback()
-        def update_call1():
-            value2= 125
-            return value2
