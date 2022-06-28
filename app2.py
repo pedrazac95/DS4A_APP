dcc3e5f3440ccac839f77457169dc875bcf13011
CAMILO PEDRAZA <ca.pedraza10@gmail.com>
2022-06-27 19:06:46 -0500

    new graphic ds4a

--- app2.py
+++ app2.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import plotly.express as px  # (version 4.7.0 or higher)
+import plotly.express as px  
 import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+from dash import Dash, dcc, html, Input, Output
 
 
 
@@ -10,41 +10,40 @@ from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0
 app = Dash(__name__)
 
 # -- Import and clean data (importing csv into pandas)
-# df = pd.read_csv("intro_bees.csv")
-#df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Other/Dash_Introduction/intro_bees.csv")
+#df = pd.read_csv("intro_bees.csv")
+df = pd.read_csv("data/conteo_union.csv")
 
-#df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
-#df.reset_index(inplace=True)
+
 #print(df[:5])
 
 # ------------------------------------------------------------------------------
 # App layout
 app.layout = html.Div([
 
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+    html.H1("DS4A TEAM 21 - La Asención", style={'text-align': 'center'}),
 
     dcc.Dropdown(id="slct_year",
                  options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
+                     {"label": "TOLIMA", "value": "TOLIMA"},
+                     {"label": "BOGOTA D.C.", "value": 'BOGOTA D.C.'},
+                     {"label": "ATLANTICO", "value": 'ATLANTICO'},
+                     {"label": "VALLE DEL CAUCA", "value": '"VALLE DEL CAUCA'}],
                  multi=False,
-                 value=2015,
+                 value='TOLIMA',
                  style={'width': "40%"}
                  ),
 
     html.Div(id='output_container', children=[]),
     html.Br(),
 
-    #dcc.Graph(id='my_bee_map', figure={})
+    dcc.Graph(id='my_bee_map', figure={})
 
 ])
 
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
-'''@app.callback(
+@app.callback(
     [Output(component_id='output_container', component_property='children'),
      Output(component_id='my_bee_map', component_property='figure')],
     [Input(component_id='slct_year', component_property='value')]
@@ -56,11 +55,17 @@ def update_graph(option_slctd):
     container = "The year chosen by user was: {}".format(option_slctd)
 
     dff = df.copy()
-    dff = dff[dff["Year"] == option_slctd]
-    dff = dff[dff["Affected by"] == "Varroa_mites"]
+    dff = dff[dff["Sucursal"] == option_slctd]
+    #dff = dff[dff["Affected by"] == "Varroa_mites"]
 
     # Plotly Express
-    fig = px.choropleth(
+
+    fig = px.line(dff[(dff['Year_Month']!='NA')], x='Year_Month', y="Count_CCont")
+
+
+
+
+    '''fig = px.choropleth(
         data_frame=dff,
         locationmode='USA-states',
         locations='state_code',
@@ -70,27 +75,27 @@ def update_graph(option_slctd):
         color_continuous_scale=px.colors.sequential.YlOrRd,
         labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
         template='plotly_dark'
+    )'''
+
+    '''Plotly Graph Objects (GO)
+    fig = go.Figure(
+        data=[go.Choropleth(
+            locationmode='USA-states',
+            locations=dff['state_code'],
+            z=dff["Pct of Colonies Impacted"].astype(float),
+            colorscale='Reds',
+        )]
     )
 
-    # Plotly Graph Objects (GO)
-    # fig = go.Figure(
-    #     data=[go.Choropleth(
-    #         locationmode='USA-states',
-    #         locations=dff['state_code'],
-    #         z=dff["Pct of Colonies Impacted"].astype(float),
-    #         colorscale='Reds',
-    #     )]
-    # )
-    #
-    # fig.update_layout(
-    #     title_text="Bees Affected by Mites in the USA",
-    #     title_xanchor="center",
-    #     title_font=dict(size=24),
-    #     title_x=0.5,
-    #     geo=dict(scope='usa'),
-    # )
+    fig.update_layout(
+        title_text="Bees Affected by Mites in the USA",
+        title_xanchor="center",
+        title_font=dict(size=24),
+        title_x=0.5,
+        geo=dict(scope='usa'),
+    )'''
 
-    return container, fig'''
+    return container, fig
 
 
 # ------------------------------------------------------------------------------
